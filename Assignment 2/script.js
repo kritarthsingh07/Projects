9fd2a60a6e260a2d22acb2286711bbfd4dcde341
Kritarth Singh <kritarthsingh87@gmail.com>
2026-02-18 21:39:37 +0530

    Update script.js

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,84 +1,55 @@
-body {
-  font-family: Arial, sans-serif;
-  padding: 20px;
+
+
+let thumbnails = document.querySelectorAll(".thumb");
+let previewImage = document.getElementById("previewImage");
+
+thumbnails.forEach((thumb) => {
+  thumb.addEventListener("click", function () {
+
+    
+    previewImage.src = this.src.replace("/100", "/400");
+
+    
+    thumbnails.forEach((img) => img.classList.remove("active"));
+
+    
+    this.classList.add("active");
+  });
+});
+
+
+
+function addRow() {
+  let name = document.getElementById("name").value;
+  let age = document.getElementById("age").value;
+  let course = document.getElementById("course").value;
+
+  if (name === "" || age === "" || course === "") {
+    alert("Please fill all fields!");
+    return;
+  }
+
+  let tableBody = document.querySelector("#studentTable tbody");
+
+  
+  let row = document.createElement("tr");
+
+  row.innerHTML = `
+    <td>${name}</td>
+    <td>${age}</td>
+    <td>${course}</td>
+    <td><button class="delete-btn" onclick="deleteRow(this)">Delete</button></td>
+  `;
+
+  tableBody.appendChild(row);
+
+  
+  document.getElementById("name").value = "";
+  document.getElementById("age").value = "";
+  document.getElementById("course").value = "";
 }
 
-h1, h2 {
-  text-align: center;
-}
 
-/* ======================= */
-/* TASK 1 IMAGE GALLERY */
-/* ======================= */
-
-.gallery-container {
-  text-align: center;
-}
-
-.preview img {
-  width: 400px;
-  height: 250px;
-  border: 3px solid black;
-  margin-bottom: 20px;
-}
-
-.thumbnails img {
-  width: 100px;
-  height: 70px;
-  margin: 10px;
-  cursor: pointer;
-  border: 3px solid transparent;
-  transition: 0.3s;
-}
-
-/* Highlight Selected Thumbnail */
-.thumbnails img.active {
-  border: 3px solid red;
-}
-
-/* ======================= */
-/* TASK 2 TABLE GENERATOR */
-/* ======================= */
-
-.form-container {
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-input {
-  padding: 8px;
-  margin: 5px;
-}
-
-button {
-  padding: 8px 15px;
-  cursor: pointer;
-}
-
-table {
-  width: 80%;
-  margin: auto;
-  border-collapse: collapse;
-}
-
-th, td {
-  padding: 12px;
-  border: 1px solid black;
-  text-align: center;
-}
-
-/* Alternate Row Colors */
-tbody tr:nth-child(even) {
-  background-color: #f2f2f2;
-}
-
-tbody tr:nth-child(odd) {
-  background-color: white;
-}
-
-.delete-btn {
-  background-color: red;
-  color: white;
-  border: none;
-  padding: 5px 10px;
+function deleteRow(button) {
+  button.parentElement.parentElement.remove();
 }
