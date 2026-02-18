355568e8f865a7d469a447498af42dd0b7b951ef
Kritarth Singh <kritarthsingh87@gmail.com>
2026-02-18 21:37:30 +0530

    Update script.js

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,16 +1,84 @@
-let thumbs = document.querySelectorAll(".thumb");
-let mainImage = document.getElementById("mainImage");
+body {
+  font-family: Arial, sans-serif;
+  padding: 20px;
+}
 
-thumbs.forEach((img) => {
-  img.addEventListener("click", function () {
+h1, h2 {
+  text-align: center;
+}
 
-    // Change Preview Image
-    mainImage.src = this.src.replace("100/70", "400/250");
+/* ======================= */
+/* TASK 1 IMAGE GALLERY */
+/* ======================= */
 
-    // Remove Highlight from All
-    thumbs.forEach((thumb) => thumb.classList.remove("active"));
+.gallery-container {
+  text-align: center;
+}
 
-    // Highlight Selected Thumbnail
-    this.classList.add("active");
-  });
-});
+.preview img {
+  width: 400px;
+  height: 250px;
+  border: 3px solid black;
+  margin-bottom: 20px;
+}
+
+.thumbnails img {
+  width: 100px;
+  height: 70px;
+  margin: 10px;
+  cursor: pointer;
+  border: 3px solid transparent;
+  transition: 0.3s;
+}
+
+/* Highlight Selected Thumbnail */
+.thumbnails img.active {
+  border: 3px solid red;
+}
+
+/* ======================= */
+/* TASK 2 TABLE GENERATOR */
+/* ======================= */
+
+.form-container {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+input {
+  padding: 8px;
+  margin: 5px;
+}
+
+button {
+  padding: 8px 15px;
+  cursor: pointer;
+}
+
+table {
+  width: 80%;
+  margin: auto;
+  border-collapse: collapse;
+}
+
+th, td {
+  padding: 12px;
+  border: 1px solid black;
+  text-align: center;
+}
+
+/* Alternate Row Colors */
+tbody tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+tbody tr:nth-child(odd) {
+  background-color: white;
+}
+
+.delete-btn {
+  background-color: red;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+}
